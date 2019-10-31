a04f7218cd0109337701e94a7878c82b433fdb91
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 19:21:55 +0300

    Scripted auto-commit on change (2019-10-31 19:21:55) by gitwatch.sh

--- src/main/java/CraftPair.java
+++ src/main/java/CraftPair.java
@@ -1,4 +1,5 @@
 import scala.Tuple2;
+import scala.Tuple3;
 
 class CraftPair {
 
@@ -7,14 +8,14 @@ class CraftPair {
         return new Tuple2<>(Integer.parseInt(pair[ParseAF.AIRPORT_ID]), pair[ParseAF.AIRPORT_NAME]);
     }
 
-    static Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> flyPair(String s) {
+    static Tuple2<Tuple2<Integer, Integer>, Tuple3<Double, Double, Double>> flyPair(String s) {
         String[] rows = ParseAF.parseFly(s);
         if (rows[ParseAF.CANCEL].equals(ParseAF.IS_CANCEL)) {
-            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(0.0, 1.0));
+            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple3<>(0.0, 1.0, 1.0));
         }
         if (rows[ParseAF.DELAY].equals(ParseAF.NOT_DELAY) || rows[ParseAF.DELAY].equals("")) {
-            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(0.0, 0.0));
+            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple3<>(0.0, 0.0, 1.0));
         }
-        return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(Double.parseDouble(rows[ParseAF.DELAY]), 1.0));
+        return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple3<>(Double.parseDouble(rows[ParseAF.DELAY]), 1.0, 1.0));
     }
 }
