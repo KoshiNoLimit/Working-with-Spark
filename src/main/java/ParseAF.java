e2658b1716c7c4c588f30b7893738ac4ba2341ac
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 16:11:27 +0300

    Scripted auto-commit on change (2019-10-31 16:11:27) by gitwatch.sh

--- src/main/java/ParseAF.java
+++ src/main/java/ParseAF.java
@@ -1,2 +1,36 @@
 public class ParseAF {
+    static final Integer AIRPORT_ID = 0;
+    static final Integer FLY_ID = 14;
+    static final Integer AIRPORT_IND = 0;
+    static final Integer FLY_IND = 1;
+    static final Integer DELAY = 18;
+    static final Integer AIRPORT_NAME = 1;
+    private static final String[] EMPTY = new String[0];
+    private static final Integer CANCEL = 19;
+    private static final String DELIMITER_FLY = ",";
+    private static final String DELIMITER_AIR = "\",\"";
+    private static final String IS_CANCEL = "1.00";
+    private static final String NOT_DELAY = "0.00";
+
+    private static final String COMMA = "\"";
+
+    private static String[] deleteCommas(String[] s) {
+        for(int i = 0; i < s.length; i++) {
+            s[i] = s[i].replaceAll(COMMA, "");
+        }
+        return s;
+    }
+
+    static String[] parseAir(String s) {
+        String[] lines = s.split(DELIMITER_AIR);
+        return deleteCommas(lines);
+    }
+
+    static String[] parseFly(String s) {
+        String[] lines = s.split(DELIMITER_FLY);
+        if(lines[CANCEL].equals(IS_CANCEL) || lines[DELAY].equals(NOT_DELAY) || lines[DELAY].equals("")) {
+            return EMPTY;
+        }
+        return deleteCommas(lines);
+    }
 }
