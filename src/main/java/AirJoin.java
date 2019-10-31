c49bf1640b9f61018cabe3d15afbbcb20e0846d1
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 21:35:46 +0300

    Scripted auto-commit on change (2019-10-31 21:35:46) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -5,7 +5,6 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 import scala.Tuple3;
-import scala.Tuple4;
 
 import java.util.Map;
 
@@ -20,7 +19,7 @@ public class AirJoin {
         JavaPairRDD<Tuple2<Integer, Integer>, Tuple3<Double, Double, Double>> keyFlightPairs = flightPairs
                 .reduceByKey((a, b)-> new Tuple3<>(Math.max(a._1(), b._1()), a._2() + b._2(), a._3() + b._3()));
         JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> sameFlightsResults = keyFlightPairs
-                .mapToPair(k -> new Tuple2<>(k._1(), new Tuple2<>(k._2()._1(), new Double(100 * (k._2()._2() / k._2()._3())))));
+                .mapToPair(k -> new Tuple2<>(k._1(), new Tuple2<>(k._2()._1(), 100 * (k._2()._2() / k._2()._3()))));
 
         JavaRDD<String> airports = sc.textFile("AIR3.csv");
         JavaPairRDD<Integer, String> airPairs = airports.mapToPair(CraftPair::airPair);
@@ -29,14 +28,14 @@ public class AirJoin {
 
         final Broadcast<Map<Integer, String>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
 
-        JavaPairRDD <Tuple2<Tuple2<Integer, Integer>, Tuple2<String, String>>, Tuple2<Double, Double>> result = sameFlightsResults
-                .flatMapToPair(s -> new Tuple2<>(
-                        new Tuple2<>(
-                                s._1(),
+        JavaPairRDD <Tuple2<Integer, Integer>, Tuple2<Tuple2<String, String>, Tuple2<Double, Double>>> result = sameFlightsResults
+                .flatMapToPair(s ->
+                        new Tuple2<>(s._1(),
                                 new Tuple2<>(
-                                    airportsBroadcasted.value().get(s._1()._1()),
-                                    airportsBroadcasted.value().get(s._1()._2()))),
-                                s._2()));
+                                        new Tuple2<>(
+                                                airportsBroadcasted.value().get(s._1()._1()),
+                                                airportsBroadcasted.value().get(s._1()._2())),
+                                        s._2())));
         result.saveAsTextFile("output");
     }
 }
