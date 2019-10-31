0f664d11bf3258b16d3bcb738baa38e984761dc2
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 19:44:49 +0300

    Scripted auto-commit on change (2019-10-31 19:44:49) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -2,7 +2,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Int;
 import scala.Tuple2;
 import scala.Tuple3;
 
@@ -16,9 +15,9 @@ public class AirJoin {
                 .mapToPair(CraftPair::flyPair);
         JavaPairRDD<Tuple2<Integer, Integer>, Tuple3<Double, Double, Double>> keyFlightPairs = flightPairs
                 .reduceByKey((a, b)-> new Tuple3<>(Math.max(a._1(), b._1()), a._2() + b._2(), a._3() + b._3()));
-        JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> sameFlightsResults = keyFlightPairs.ma
-
-
+        JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> sameFlightsResults = keyFlightPairs
+                .mapToPair(k -> new Tuple2<>(k._1, new Tuple2<>(k._2()._1(), 100 * (k._2()._2() / k._2()._3()))));
+        
         JavaRDD<String> airports = sc.textFile("AIR3.csv");
         JavaPairRDD<Integer, String> airPairs = airports.mapToPair(CraftPair::airPair);
 
