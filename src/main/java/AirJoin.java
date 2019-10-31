39fe8c5f1fea4f00f4b143e2378cb97705331252
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 19:29:25 +0300

    Scripted auto-commit on change (2019-10-31 19:29:25) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -11,7 +11,8 @@ public class AirJoin {
 
         JavaRDD<String> flights = sc.textFile("TIME3.csv");
         JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> flightPairs = flights.mapToPair(CraftPair::flyPair);
-        JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> keyFlightPairs = flightPairs.reduceByKey(())
+        JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> keyFlightPairs = flightPairs
+                .reduceByKey((a, b)-> new Tuple2<>())
 
         JavaRDD<String> airports = sc.textFile("AIR3.csv");
         JavaPairRDD<Integer, String> airPairs = airports.mapToPair(CraftPair::airPair);
