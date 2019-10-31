547828210df0312424db0b31a1c8d745429c5928
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 21:04:38 +0300

    Scripted auto-commit on change (2019-10-31 21:04:38) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -20,7 +20,7 @@ public class AirJoin {
         JavaPairRDD<Tuple2<Integer, Integer>, Tuple3<Double, Double, Double>> keyFlightPairs = flightPairs
                 .reduceByKey((a, b)-> new Tuple3<>(Math.max(a._1(), b._1()), a._2() + b._2(), a._3() + b._3()));
         JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> sameFlightsResults = keyFlightPairs
-                .mapToPair(k -> new Tuple2<>(k._1, new Tuple2<>(k._2()._1(), 100 * (k._2()._2() / k._2()._3()))));
+                .mapToPair(k -> new Tuple2<>(k._1(), new Tuple2<>(k._2()._1(), 100 * (k._2()._2() / k._2()._3()))));
 
         JavaRDD<String> airports = sc.textFile("AIR3.csv");
         JavaPairRDD<Integer, String> airPairs = airports.mapToPair(CraftPair::airPair);
@@ -29,8 +29,12 @@ public class AirJoin {
 
         final Broadcast<Map<Integer, String>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
 
-        JavaPairRDD<Tuple2<Integer, Integer>, Tuple4<String, String, Double, Double>> result = sameFlightsResults
-                .flatMapToPair(s -> new Tuple2<>(s._1, new Tuple4<>(airportsBroadcasted.getValue(s._1._1, ))))
+        JavaPairRDD <Tuple2<Integer, Integer>, Tuple2<String, String>, Tuple2<Double, Double>> result = sameFlightsResults
+                .flatMapToPair(s -> new Tuple3<>(s._1(),
+                        new Tuple2<>(airportsBroadcasted.value().get(s._1()._1()), airportsBroadcasted.value().get(s._1()._2())),
+                        s._1(),
+                        s._2()
+                ));
 
 
 
