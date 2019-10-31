51891c609f02f67679421f512968564f65298622
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 17:10:42 +0300

    Scripted auto-commit on change (2019-10-31 17:10:42) by gitwatch.sh

--- src/main/java/CraftPair.java
+++ src/main/java/CraftPair.java
@@ -6,6 +6,6 @@ public class CraftPair {
         return new Tuple2<>(Integer.parseInt(pair[ParseAF.AIRPORT_ID]), pair[ParseAF.AIRPORT_NAME]);
     }
 
-    public static Tuple2<Tuple2<Integer, Integer>, >
+    public static Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>>
 
 }
