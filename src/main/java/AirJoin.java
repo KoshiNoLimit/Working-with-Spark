da57e882da1af65e96d12002894e54d385610b5c
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 19:34:21 +0300

    Scripted auto-commit on change (2019-10-31 19:34:21) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -2,7 +2,9 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Int;
 import scala.Tuple2;
+import scala.Tuple3;
 
 public class AirJoin {
     public static void main(String[] args) {
@@ -10,9 +12,12 @@ public class AirJoin {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> flights = sc.textFile("TIME3.csv");
-        JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> flightPairs = flights.mapToPair(CraftPair::flyPair);
-        JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> keyFlightPairs = flightPairs
-                .reduceByKey((a, b)-> new Tuple2<>())
+        JavaPairRDD<Tuple2<Integer, Integer>, Tuple3<Double, Double, Double>> flightPairs = flights
+                .mapToPair(CraftPair::flyPair);
+        JavaPairRDD<Tuple2<Integer, Integer>, Tuple3<Double, Double, Double>> keyFlightPairs = flightPairs
+                .reduceByKey((a, b)-> new Tuple3<>(Math.max(a._1(), b._1()), a._2() + b._2(), a._3() + b._3()));
+        JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> sameFlightsResults = keyFlightPairs.
+
 
         JavaRDD<String> airports = sc.textFile("AIR3.csv");
         JavaPairRDD<Integer, String> airPairs = airports.mapToPair(CraftPair::airPair);
