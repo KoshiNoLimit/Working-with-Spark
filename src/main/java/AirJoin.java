24732a9f9dbe68ae722921324c1cd3a74c368f6b
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 15:40:25 +0300

    Scripted auto-commit on change (2019-10-31 15:40:25) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -1,10 +1,16 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirJoin {
     public static void main(String[] args) {
         SparkConf conf  = new SparkConf().setAppName("L3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> flights = sc.textFile("TIME3.csv");
+
+        JavaRDD<String> airports = sc.textFile("AIR3.csv");
+
 
     }
 }
