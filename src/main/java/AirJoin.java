412500fc09be4eae9d2d270ada42ee24e69acf7e
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 18:48:40 +0300

    Scripted auto-commit on change (2019-10-31 18:48:40) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -2,8 +2,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Int;
-import scala.Tuple1;
 import scala.Tuple2;
 
 public class AirJoin {
--- src/main/java/CraftPair.java
+++ src/main/java/CraftPair.java
@@ -10,18 +10,11 @@ class CraftPair {
     static Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> flyPair(String s) {
         String[] rows = ParseAF.parseFly(s);
         if (rows[ParseAF.CANCEL].equals(ParseAF.IS_CANCEL)) {
-            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(0, 1));
+            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(0.0, 1.0));
         }
         if (rows[ParseAF.DELAY].equals(ParseAF.NOT_DELAY) || rows[ParseAF.DELAY].equals("")) {
-            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(0, 0));
+            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(0.0, 0.0));
         }
-
-        return 
-
-
-
-
-        return null;
+        return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(Double.parseDouble(rows[ParseAF.DELAY]), 1.0));
     }
-
 }
