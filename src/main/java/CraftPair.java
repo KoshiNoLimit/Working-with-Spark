5c83420d3d4ab77f715e497ad0c96a78a5d7a4e4
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 18:17:21 +0300

    Scripted auto-commit on change (2019-10-31 18:17:21) by gitwatch.sh

--- src/main/java/CraftPair.java
+++ src/main/java/CraftPair.java
@@ -1,6 +1,9 @@
 import scala.Tuple2;
 
 class CraftPair {
+    
+    private static final bool INTIME = 1;
+
     static Tuple2<Integer, String> airPair(String s) {
         String[] pair = ParseAF.parseAir(s);
         return new Tuple2<>(Integer.parseInt(pair[ParseAF.AIRPORT_ID]), pair[ParseAF.AIRPORT_NAME]);
@@ -9,9 +12,11 @@ class CraftPair {
     static Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> flyPair(String s) {
         String[] rows = ParseAF.parseFly(s);
         if (rows[ParseAF.CANCEL].equals(ParseAF.IS_CANCEL)) {
-            return new Tuple2<>(new Tuple2<>(rows[ParseAF.F_FIRST_AIR_ID], rows[ParseAF.F_SECOND_AIR_ID]), new Tuple2<>(0, 1));
+            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple2<>(0, 1));
         }
-        
+
+
+
 
 
         return null;
