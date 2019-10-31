198b0520d3fc0bfdea9452b8d65f9a7f84af8789
KoshiNoLimit <zmsh04@gmail.com>
2019-10-31 16:20:53 +0300

    Scripted auto-commit on change (2019-10-31 16:20:53) by gitwatch.sh

--- src/main/java/craftPair.java
+++ src/main/java/craftPair.java
@@ -3,7 +3,7 @@ import scala.Tuple2;
 public class craftPair {
     public static Tuple2<Integer, String> airPair(String s) {
         String[] pair = ParseAF.parseAir(s);
-        return new Tuple2<>(Integer.parseInt(pair[]), pair[])
+        return new Tuple2<>(Integer.parseInt(pair[ParseAF.AIRPORT_ID]), pair[ParseAF.AIRPORT_NAME]);
     }
 
 }
