be9d6da9d0d3db98a63bf806d528219f2d708d71
KoshiNoLimit <zmsh04@gmail.com>
2019-11-01 23:14:05 +0300

    Scripted auto-commit on change (2019-11-01 23:14:05) by gitwatch.sh

--- src/main/java/CraftPair.java
+++ src/main/java/CraftPair.java
@@ -11,11 +11,17 @@ class CraftPair {
     static Tuple2<Tuple2<Integer, Integer>, Tuple3<Double, Double, Double>> flyPair(String s) {
         String[] rows = ParseAF.parseFly(s);
         if (rows[ParseAF.CANCEL].equals(ParseAF.IS_CANCEL)) {
-            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple3<>(0.0, 1.0, 1.0));
+            return new Tuple2<>(
+                    new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])),
+                    new Tuple3<>(0.0, 1.0, 1.0));
         }
         if (rows[ParseAF.DELAY].equals(ParseAF.NOT_DELAY) || rows[ParseAF.DELAY].equals("")) {
-            return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple3<>(0.0, 0.0, 1.0));
+            return new Tuple2<>(
+                    new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])),
+                    new Tuple3<>(0.0, 0.0, 1.0));
         }
-        return new Tuple2<>(new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])), new Tuple3<>(Double.parseDouble(rows[ParseAF.DELAY]), 1.0, 1.0));
+        return new Tuple2<>(
+                new Tuple2<>(Integer.parseInt(rows[ParseAF.F_FIRST_AIR_ID]), Integer.parseInt(rows[ParseAF.F_SECOND_AIR_ID])),
+                new Tuple3<>(Double.parseDouble(rows[ParseAF.DELAY]), 1.0, 1.0));
     }
 }
--- src/main/java/ParseAF.java
+++ src/main/java/ParseAF.java
@@ -2,19 +2,13 @@ class ParseAF {
     static final Integer AIRPORT_ID = 0;
     static final Integer F_FIRST_AIR_ID = 11;
     static final Integer F_SECOND_AIR_ID = 14;
-
-    static final Integer FLY_ID = 14;
-    static final Integer AIRPORT_IND = 0;
-    static final Integer FLY_IND = 1;
     static final Integer DELAY = 18;
     static final Integer AIRPORT_NAME = 1;
-    private static final String[] EMPTY = new String[0];
     static final Integer CANCEL = 19;
-    private static final String DELIMITER_FLY = ",";
-    private static final String DELIMITER_AIR = "\",\"";
     static final String IS_CANCEL = "1.00";
     static final String NOT_DELAY = "0.00";
-
+    private static final String DELIMITER_FLY = ",";
+    private static final String DELIMITER_AIR = "\",\"";
     private static final String COMMA = "\"";
 
     private static String[] deleteCommas(String[] s) {
